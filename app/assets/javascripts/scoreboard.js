af7a6c8602f4f528c54db795dfc15f44dd1b8909
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-12-12 22:48:39 -0800

    Leader Board shows and sorts by course radio button group using Ajax, add link from home page

--- app/assets/javascripts/scoreboard.js
+++ app/assets/javascripts/scoreboard.js
@@ -24,10 +24,21 @@ $(".scores.index").ready(function() {
 //	alert(course_number);
 //	alert($('#course-number').val());
 	$('#show').change(function() {
-		$('#course').val($('#course-number').text());
+	//	$('#course').val($('#course-number').text());
 	//	alert("ha");
 	//	alert($(this).val());
 		$('#show-number').submit();
+	});
+
+	$(".course-btns .btn").click(function() {	
+		if(!$(this).hasClass("active")) {
+			$(".course-btns .active input").prop('checked', false);
+			$(".course-btns .active").removeClass("active");
+			$(this).addClass("active");
+			$(".course-btns .active input").prop('checked', true);
+	//		alert($('input[name=course]:checked', '#show-number').val())
+			$('#show-number').submit();
+		}
 	});
 
 });
