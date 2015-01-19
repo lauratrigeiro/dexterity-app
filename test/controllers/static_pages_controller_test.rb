6f5403073d80828167dc0ef5549810c700cb12d1
Laura Trigeiro <lauratrigeiro@gmail.com>
2015-01-18 21:35:25 -0800

    add controller tests, update fixtures

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,12 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
 	@base_title = "Dexterity App"
+  @user = users(:tom)
 end
 
-  test "should get home" do
+  test "should get home for user that is not logged in" do
     get :home
     assert_response :success
     assert_select "title", @base_title
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", new_user_registration_path, count: 2
+    assert_select "a[href=?]", new_user_session_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", course_1_path, count: 0
   end
 
   test "should get about" do
@@ -18,6 +24,22 @@ end
     assert_select "title", "About | #{@base_title}"
   end
 
-
+  test "layout links for user that is logged in" do
+    sign_in @user
+    get :home
+    assert_response :success
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", new_user_registration_path, count: 0
+    assert_select "a[href=?]", new_user_session_path, count: 0
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", course_1_path
+    assert_select "a[href=?]", course_2_path
+    assert_select "a[href=?]", course_3_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", scores_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_registration_path
+    assert_select "a[href=?]", destroy_user_session_path
+  end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,56 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
+  
+  def setup
+  	@user = users(:tom)
+  	@other_user = users(:julia)
+  	@no_scores_user = users(:afif)
+  	@score = @user.scores.first
+  end
+
+  test "should redirect index when not logged in" do
+  	get :index
+  	assert_redirected_to new_user_session_url
+  end
+
+  test "should redirect show when not logged in" do
+  	get :show, id: @user
+  	assert_redirected_to new_user_session_url
+  end
+
+  test "should redirect show when logged in as wrong user" do
+  	sign_in @other_user
+  	get :show, id: @user
+  	assert_redirected_to root_url
+  end
+
+  test "should get index when logged in" do
+	sign_in @user
+	get :index
+	assert_response :success
+	assert_select "table", true
+	assert_select "a[href=?]", user_path(@user), count: 2
+	assert_select "a[href=?]", user_path(@other_user)
+	assert_match @other_user.email, response.body
+	assert_match @other_user.created_at.strftime("%B %e, %Y"), response.body
+  end
+
+  test "should get show when logged in, but don't display table if no scores" do
+	sign_in @no_scores_user
+	get :show, id: @no_scores_user
+	assert_response :success
+	assert_select "h1", "My Times"
+	assert_select "table", false
+  end
+
+  test "should get show when logged in and display table if scores" do
+	sign_in @user
+	get :show, id: @user
+	assert_response :success
+	assert_select "h1", "My Times"
+	assert_select "table", true
+	assert_match '%.3f' % (@score.time / 1000.0).to_s, response.body
+  end
 
 end
